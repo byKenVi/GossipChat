c9eab600f9adc443bf377032878ac9e5d0a5f994
Prunelle <prunelledalmeida500@gmail.com>
2025-06-27 11:44:29 +0200

    test

--- index.php
+++ index.php
@@ -7,6 +7,34 @@
     <title>Bienvenu sur GossipChat</title>
 </head>
 <body>
-    <h1>GossipChat</h1>
+    
+        <header>
+        <div class="sideback">
+            <img src="assets/images/logochat.jpg" alt="le logo de notre site" id="logo">
+            <h3>Concepteur</h3>
+            <h3>Learn more</h3>
+            <a href="inscription.php"><button class="btn">S'inscrire</button></a>
+            <a href="conexion.php"><button class="btn">Se Connecter</button></a>
+        </div>
+    </header>
+     <main>
+        <div class="bord">
+            <div class="name">
+                  
+                <h1>UNFORGETTABLE DISCUSSIONS</h1>
+                <h1>WEAVING CONNECTIONS</h1>
+                <h1>HAVE MEMORIES</h1>
+                    <h2>WITH</h2>
+                    <em>GossipCh@t</em>
+                    <div class="top"> <p>"Get connected,it's time to join us and stay connected"!!</p></div>
+            </div>
+            <img src="assets/images/img ac.jpg" alt="simulation" id="media">
+        </div>
+    
+
+     </main>
+     <footer>@Copiryght</footer>
+
+    
 </body>
 </html>
